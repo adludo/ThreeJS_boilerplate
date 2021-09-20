9a71d4e14f159510b00ae8d6a6a3a06bdccee5ba
adludo <a_tsai5@hotmail.com>
2021-09-20 23:17:17 +1000

    first actual commit

--- public/script.js
+++ public/script.js
@@ -1,22 +1,5 @@
 const scene = new THREE.Scene()
 
-// renderer.setSize( window.innerWidth, window.innerHeight )
-// document.body.appendChild( renderer.domElement )
-
-// // New code
-// var scene = new THREE.Scene();
-
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-// Rotating cube
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const material = new THREE.MeshLambertMaterial( { color: 0xfb8e00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
 // // Adding a rectangular prism
 const geometry = new THREE.BoxGeometry( 3, 1, 3 );
 const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
@@ -34,8 +17,6 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
 directionalLight.position.set(10, 20, 0);
 scene.add(directionalLight);
 
-
-
 // Camera
 const width = 10;
 const height = width * (window.innerHeight/window.innerWidth);
@@ -52,9 +33,6 @@ camera.position.set(4, 4, 4);
 camera.lookAt(org_vector);
 console.log(mesh.position.x, mesh.position.y, mesh.position.z);
 console.log(camera.position.x, camera.position.y, camera.position.z);
-
-console.log("testing");
-
 // var camera = new THREE.PerspectiveCamera( 
 //     75, // vertical field of view
 //     window.innerWidth/window.innerHeight, // aspect ratio
@@ -74,10 +52,6 @@ renderer.render(scene, camera);
 
 var animate = function () {
 	requestAnimationFrame( animate );
-
-	// cube.rotation.x += 0.1;
-	// cube.rotation.y += 0.1;
-
 	renderer.render(scene, camera);
 };
 
